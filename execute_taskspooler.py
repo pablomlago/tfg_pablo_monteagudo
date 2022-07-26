f47016d2f3033119529f72e857163efb3ef0cb38
efren.rama <efren.rama.maneiro@usc.es>
2022-07-26 12:39:35 +0200

    Add taskspooler mode

---
+++ execute_taskspooler.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="Prepare to execute in CESGA")
+parser.add_argument("--train", action="store_true")
+parser.add_argument("--postprocessing", type=str, required=True, choices=["beam", "beam_length_normalized", "beam_monteagudo", "argmax", "random"])
+parser.add_argument("--disable_attention", required=False, action="store_true")
+parser.add_argument("--optimize_beam_width", required=False, action="store_true")
+parser.add_argument("--store_prefixes_in_results", required=False, action="store_true")
+args = parser.parse_args()
+
+for file in os.listdir("./data/data_complete"):
+    if "csv" in file:
+        base = file.replace(".csv", "")
+        for i in range(5):
+            fold_num = i
+            command = f"tsp ./execute_job_taskspooler.sh --dataset {base} --execution_id EXPERIMENTACION_TFG --fold_num {i}"
+            if args.train:
+                command += " --train"
+            if args.postprocessing:
+                command += f" --postprocessing {args.postprocessing}"
+            if args.disable_attention:
+                command += " --disable_attention"
+            if args.optimize_beam_width:
+                command += " --optimize_beam_width"
+            if args.store_prefixes_in_results:
+                command += " --store_prefixes_in_results"
+            print("==========")
+            print(command)
+            os.system(command)
+            print("==========")
